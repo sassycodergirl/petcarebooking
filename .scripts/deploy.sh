439cee2241881d6d7ad61a345935e63da31e9796
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-15 05:03:03 +0530

    Test webhook auto-deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,28 +1,27 @@
-
 #!/bin/bash
 set -e
 
 echo "Deployment started ..."
 
-# Turn ON Maintenance Mode or return true
-# if already is in maintenance mode
+# Turn ON Maintenance Mode (optional)
 (php artisan down) || true
 
 # Pull the latest version of the app
 git pull origin main
 
 # Install composer dependencies
-composer install --optimize-autoloader --no-dev --no-interaction
+composer install --optimize-autoloader --no-dev --no-interaction --ignore-platform-reqs
 
-# Clearing Cache
+# Clear and rebuild cache
 php artisan cache:clear
 php artisan config:clear
-
-# Recreate cache
 php artisan optimize
 
-# Run database migrations
-php artisan migrate --force
+# Fix permissions for storage and bootstrap/cache
+chown -R www-data:www-data /home/coderbeans.shop/public_html/furry-and-friends/storage
+chown -R www-data:www-data /home/coderbeans.shop/public_html/furry-and-friends/bootstrap/cache
+chmod -R 775 /home/coderbeans.shop/public_html/furry-and-friends/storage
+chmod -R 775 /home/coderbeans.shop/public_html/furry-and-friends/bootstrap/cache
 
 # Turn OFF Maintenance mode
 php artisan up
