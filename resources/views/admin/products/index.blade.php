e02ac0c1c4f9d6e7267a9a7c164640a79fb648b4
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-16 01:04:49 +0530

    Product Variant implement

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -46,11 +46,61 @@
                             @endif
                         </td>
                         <td>
-                            <a href="{{ route('admin.products.edit', $product) }}" class="btn btn-sm btn-warning">Edit</a>
+                            <a href="{{ route('admin.products.edit', $product) }}"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1200 1200"><path fill="currentColor" d="M0 0v1200h1200V424.292l-196.875 196.875v381.958h-806.25v-806.25h381.958L775.708 0zm1050 0l-76.831 76.831l150 150L1200 150zM936.914 113.086L497.168 552.832l150 150l439.746-439.746zM441.943 622.339c-2.225.034-4.493.195-6.738.366v142.09h142.09c0-38.708-18.492-78.039-47.314-105.542c-23.842-22.751-54.675-37.428-88.038-36.914"/></svg></a>
                             <form action="{{ route('admin.products.destroy', $product) }}" method="POST" class="d-inline" onsubmit="return confirm('Delete this product?');">
                                 @csrf
                                 @method('DELETE')
-                                <button class="btn btn-sm btn-danger" type="submit">Delete</button>
+                                <!-- <button class="btn btn-sm btn-danger" type="submit">Delete</button> -->
+                               
+                                <button class="bin-button" type="submit">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 39 7"
+                                    class="bin-top"
+                                >
+                                    <line stroke-width="4" stroke="white" y2="5" x2="39" y1="5"></line>
+                                    <line
+                                    stroke-width="3"
+                                    stroke="white"
+                                    y2="1.5"
+                                    x2="26.0357"
+                                    y1="1.5"
+                                    x1="12"
+                                    ></line>
+                                </svg>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 33 39"
+                                    class="bin-bottom"
+                                >
+                                    <mask fill="white" id="path-1-inside-1_8_19">
+                                    <path
+                                        d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+                                    ></path>
+                                    </mask>
+                                    <path
+                                    mask="url(#path-1-inside-1_8_19)"
+                                    fill="white"
+                                    d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+                                    ></path>
+                                    <path stroke-width="4" stroke="white" d="M12 6L12 29"></path>
+                                    <path stroke-width="4" stroke="white" d="M21 6V29"></path>
+                                </svg>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 89 80"
+                                    class="garbage"
+                                >
+                                    <path
+                                    fill="white"
+                                    d="M20.5 10.5L37.5 15.5L42.5 11.5L51.5 12.5L68.75 0L72 11.5L79.5 12.5H88.5L87 22L68.75 31.5L75.5066 25L86 26L87 35.5L77.5 48L70.5 49.5L80 50L77.5 71.5L63.5 58.5L53.5 68.5L65.5 70.5L45.5 73L35.5 79.5L28 67L16 63L12 51.5L0 48L16 25L22.5 17L20.5 10.5Z"
+                                    ></path>
+                                </svg>
+                                </button>
+
                             </form>
                         </td>
                     </tr>
