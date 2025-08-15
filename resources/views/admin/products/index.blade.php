35feb00cdcff803cc5f1e0089bdc66a249c7c873
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-16 01:22:00 +0530

    Product Variant implement

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -47,7 +47,7 @@
                         </td>
                         <td>
                             <a href="{{ route('admin.products.edit', $product) }}"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1200 1200"><path fill="#438533" d="M0 0v1200h1200V424.292l-196.875 196.875v381.958h-806.25v-806.25h381.958L775.708 0zm1050 0l-76.831 76.831l150 150L1200 150zM936.914 113.086L497.168 552.832l150 150l439.746-439.746zM441.943 622.339c-2.225.034-4.493.195-6.738.366v142.09h142.09c0-38.708-18.492-78.039-47.314-105.542c-23.842-22.751-54.675-37.428-88.038-36.914"/></svg></a>
-                            <form action="{{ route('admin.products.destroy', $product) }}" method="POST" class="d-inline" onsubmit="return confirm('Delete this product?');">
+                           |  <form action="{{ route('admin.products.destroy', $product) }}" method="POST" class="d-inline" onsubmit="return confirm('Delete this product?');">
                                 @csrf
                                 @method('DELETE')
                                 <!-- <button class="btn btn-sm btn-danger" type="submit">Delete</button> -->
