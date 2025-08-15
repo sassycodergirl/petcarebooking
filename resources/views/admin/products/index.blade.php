344663d4ef6af55ee3f1a11d6c99653421cd9e7d
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-16 01:17:50 +0530

    Product Variant implement

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -53,52 +53,7 @@
                                 <!-- <button class="btn btn-sm btn-danger" type="submit">Delete</button> -->
                                
                                 <button class="bin-button" type="submit">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 39 7"
-                                    class="bin-top"
-                                >
-                                    <line stroke-width="4" stroke="red" y2="5" x2="39" y1="5"></line>
-                                    <line
-                                    stroke-width="3"
-                                    stroke="red"
-                                    y2="1.5"
-                                    x2="26.0357"
-                                    y1="1.5"
-                                    x1="12"
-                                    ></line>
-                                </svg>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 33 39"
-                                    class="bin-bottom"
-                                >
-                                    <mask fill="red" id="path-1-inside-1_8_19">
-                                    <path
-                                        d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
-                                    ></path>
-                                    </mask>
-                                    <path
-                                    mask="url(#path-1-inside-1_8_19)"
-                                    fill="red"
-                                    d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-                                    ></path>
-                                    <path stroke-width="4" stroke="red" d="M12 6L12 29"></path>
-                                    <path stroke-width="4" stroke="red" d="M21 6V29"></path>
-                                </svg>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 89 80"
-                                    class="garbage"
-                                >
-                                    <path
-                                    fill="red"
-                                    d="M20.5 10.5L37.5 15.5L42.5 11.5L51.5 12.5L68.75 0L72 11.5L79.5 12.5H88.5L87 22L68.75 31.5L75.5066 25L86 26L87 35.5L77.5 48L70.5 49.5L80 50L77.5 71.5L63.5 58.5L53.5 68.5L65.5 70.5L45.5 73L35.5 79.5L28 67L16 63L12 51.5L0 48L16 25L22.5 17L20.5 10.5Z"
-                                    ></path>
-                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#e70000" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>
                                 </button>
 
                             </form>
