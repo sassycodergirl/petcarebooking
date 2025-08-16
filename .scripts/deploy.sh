76aaf1e7846000e1a145d6f0d6e0a5584dbec329
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-16 09:07:27 +0530

    Product Variant implement

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -35,16 +35,21 @@ PROJECT_PATH="/home/coderbeans.shop/public_html/furry-and-friends"
 chmod o+x /home /home/coderbeans.shop /home/coderbeans.shop/public_html
 chmod o+x "$PROJECT_PATH"
 
-# Laravel writable folders
-chown -R nobody:nogroup "$PROJECT_PATH/storage" "$PROJECT_PATH/bootstrap/cache"
-chmod -R 777 "$PROJECT_PATH/storage" "$PROJECT_PATH/bootstrap/cache"
+# -----------------------------
+# Storage & Cache
+# -----------------------------
 
-
+# Create necessary storage/framework folders
 mkdir -p "$PROJECT_PATH/storage/framework/sessions" \
          "$PROJECT_PATH/storage/framework/views" \
          "$PROJECT_PATH/storage/framework/cache"
-chown -R nobody:nogroup "$PROJECT_PATH/storage/framework"
-chmod -R 777 "$PROJECT_PATH/storage/framework"
+
+
+# Ensure proper ownership (nobody = web server)
+chown -R nobody:nogroup "$PROJECT_PATH/storage" "$PROJECT_PATH/bootstrap/cache"
+
+# Set safe writable permissions
+chmod -R 777 "$PROJECT_PATH/storage" "$PROJECT_PATH/bootstrap/cache"
 
 
 # Ensure laravel.log exists
@@ -53,19 +58,6 @@ touch "$PROJECT_PATH/storage/logs/laravel.log"
 chmod 777 "$PROJECT_PATH/storage/logs/laravel.log"
 chown nobody:nogroup "$PROJECT_PATH/storage/logs/laravel.log"
 
-
-# Make them writable
-mkdir -p "$PROJECT_PATH/public/products"
-mkdir -p "$PROJECT_PATH/public/product-variants"
-mkdir -p "$PROJECT_PATH/public/variant-gallery"
-
-
-# chmod -R 777 "$PROJECT_PATH/public/products"
-# chmod -R 777 "$PROJECT_PATH/public/product-variants"
-# chmod -R 777 "$PROJECT_PATH/public/variant-gallery"
-# chown -R nobody:nogroup "$PROJECT_PATH/public/products"
-# chown -R nobody:nogroup "$PROJECT_PATH/public/product-variants"
-# chown -R nobody:nogroup "$PROJECT_PATH/public/variant-gallery"
 
 # Make directories if they don't exist
 mkdir -p "$PROJECT_PATH/public/products" \
