16a72face250346b850e2979e87faae048c580e6
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-16 11:24:13 +0530

    Product Variant implement

--- app/Http/Controllers/Admin/ProductVariantGalleryController.php
+++ app/Http/Controllers/Admin/ProductVariantGalleryController.php
@@ -10,13 +10,17 @@ class ProductVariantGalleryController extends Controller
     public function destroy($id)
     {
         $image = ProductVariantImage::findOrFail($id);
+        $variant = $image->variant;
 
-        // Delete the file if exists
+        // Delete the physical image file if it exists
         if ($image->image && file_exists(public_path($image->image))) {
             unlink(public_path($image->image));
         }
 
+        // Delete the database record
         $image->delete();
+
+        // Variant and Product remain intact, even if this was the last image
 
         return back()->with('success', 'Variant image deleted successfully.');
     }
