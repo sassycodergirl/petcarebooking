9843452bf7481b1d7b56a863e0c95ad28e7fc3b6
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-17 17:32:33 +0530

    frontend ecom setup

--- resources/views/frontend/shop/products.blade.php
+++ resources/views/frontend/shop/products.blade.php
@@ -17,24 +17,33 @@
 
 <div class="container py-5">
 
-    <!-- Filter Pills -->
-    <div class="mb-4">
-        <h5>Filter by:</h5>
-        <div class="btn-group" role="group" aria-label="Attributes">
-            @foreach(\App\Models\Attribute::all() as $attribute)
-                <input type="checkbox" class="filter-attribute" value="{{ $attribute->id }}" id="attr-{{ $attribute->id }}">
-                <label for="attr-{{ $attribute->id }}" class="btn btn-outline-primary">
-                    {{ $attribute->name }}
-                </label>
-            @endforeach
+    <div class="row">
+        <div class="col-md-3">
+              <h5 class="mb-4">{{ $category->name }}</h5>
+        </div>
+        <div class="col-md-9">
+            <!-- Filter Pills -->
+            <div class="mb-4">
+                <h5>Filter by:</h5>
+                <div class="btn-group" role="group" aria-label="Attributes">
+                    @foreach(\App\Models\Attribute::all() as $attribute)
+                        <input type="checkbox" class="filter-attribute" value="{{ $attribute->id }}" id="attr-{{ $attribute->id }}">
+                        <label for="attr-{{ $attribute->id }}" class="btn btn-outline-primary">
+                            {{ $attribute->name }}
+                        </label>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
+
+    
 
     <div class="row">
         <!-- Sidebar with siblings (other subcategories of parent) -->
         <div class="col-md-3">
             @if($category->parent)
-                <h4 class="mb-3">{{ $category->parent->name }}</h4>
+                <!-- <h4 class="mb-3">{{ $category->parent->name }}</h4> -->
                 <ul class="list-group">
                     @foreach($category->parent->children as $sibling)
                         <li class="list-group-item {{ $sibling->id === $category->id ? 'active' : '' }}">
@@ -49,7 +58,7 @@
 
         <!-- Products list -->
         <div class="col-md-9">
-            <h2 class="mb-4">{{ $category->name }}</h2>
+          
 
             <div id="productsGrid">
                 @include('frontend.shop.partials.products-grid', ['products' => $products])
