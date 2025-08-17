6029c0ce022ec0968f0e7b25c392101ea769f496
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-18 00:32:16 +0530

    frontend ecom setup

--- resources/views/frontend/shop/category.blade.php
+++ resources/views/frontend/shop/category.blade.php
@@ -123,7 +123,7 @@
                     <div class="row">
                         @foreach($products as $product)
                             <div class="col-6 col-md-3 mb-4 new-product pb-0">
-                                <div class="product-card-col h-100 shadow-sm p-4">
+                                <div class="product-card-col h-100 shadow-sm p-2 p-md-4">
                                     @if($product->image)
                                     <a href="{{ route('product.show', $product->slug) }}" class="product-card-img p-0">
                                     <div class="product-img h-100">
@@ -136,7 +136,7 @@
                                         <h3><a href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a></h3>
                                         <div class="">
                                             <p class="card-text">₹{{ $product->price }}</p>
-                                            <button class="add-to-bag cd-button" data-id="{{ $product->id }}"  data-name="{{ $product->name }}"  data-price="{{ $product->price }}" data-image="{{ asset('public/' . $product->image) }}"><img src="{{ asset('images/bag-icon.svg') }}" alt=""> Add to Cart</button>
+                                            <button class="add-to-bag cd-button" data-id="{{ $product->id }}"  data-name="{{ $product->name }}"  data-price="{{ $product->price }}" data-image="{{ asset('public/' . $product->image) }}"><img class="d-none d-md-block" src="{{ asset('images/bag-icon.svg') }}" alt=""> Add to Cart</button>
                                         </div>
                                         
                                     </div>
