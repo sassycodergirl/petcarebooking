512149eb763f37f41667bd9424384e67e312faa0
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-18 00:20:40 +0530

    frontend ecom setup

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/logo.png')}}">
     <title>Furry & Friend</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
