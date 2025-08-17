6dcb2d10e3ad61000e548f555f2667812e40bae2
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-18 00:01:32 +0530

    frontend ecom setup

--- resources/views/frontend/shop/partials/products-grid.blade.php
+++ resources/views/frontend/shop/partials/products-grid.blade.php
@@ -1,24 +1,27 @@
 @if($products->count())
     <div class="row">
         @foreach($products as $product)
-              <div class="col-md-4 mb-4 new-product pb-0">
+               <div class="col-md-4 mb-4 new-product pb-0">
                                 <div class="product-card-col h-100 shadow-sm p-4">
                                     @if($product->image)
-                                    <a href="#" class="product-card-img">
-                                    <div class="product-img">
+                                    <a href="#" class="product-card-img p-0">
+                                    <div class="product-img h-100">
 
-                                        <img src="{{ $product->image }}" class="img-fluid " alt="{{ $product->name }}">
+                                        <img src="{{ $product->image }}" class="img-fluid h-100" alt="{{ $product->name }}">
                                     </div>
                                     </a>
                                     @endif
-                                    <div class="card-body text-center">
+                                    <div class="card-body text-center px-4">
                                         <h3><a href="#">{{ $product->name }}</a></h3>
-                                       
-                                        <p class="card-text">₹{{ $product->price }}</p>
-                                        <button class="add-to-bag cd-button" data-id="{{ $product->id }}"  data-name="{{ $product->name }}"  data-price="{{ $product->price }}" data-image="{{ asset('public/' . $product->image) }}"><img src="{{ asset('images/bag-icon.svg') }}" alt=""> Add to Cart</button>
+                                        <div class="">
+                                            <p class="card-text">₹{{ $product->price }}</p>
+                                            <button class="add-to-bag cd-button" data-id="{{ $product->id }}"  data-name="{{ $product->name }}"  data-price="{{ $product->price }}" data-image="{{ asset('public/' . $product->image) }}"><img src="{{ asset('images/bag-icon.svg') }}" alt=""> Add to Cart</button>
+                                        </div>
+                                        
                                     </div>
                                 </div>
                             </div>
+           
         @endforeach
     </div>
 
