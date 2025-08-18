8b16c2ae83a51053ddc9dc8436da325cf2313ced
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-18 05:39:10 +0530

    frontend ecom setup

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -216,7 +216,7 @@ $(document).ready(function(){
 
     // Re-init slick
     galleryThumbs.slick({
-    slidesToShow:3,
+    slidesToShow:4,
     slidesToScroll: 1,
     asNavFor: '.gallery-main',
     focusOnSelect: true,
