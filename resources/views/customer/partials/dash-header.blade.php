8ccb61965cf98354e5f33b1ef501da181ef970f2
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 20:17:32 +0530

    customer dashboard

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -26,12 +26,12 @@
   </head>
   <body>
     <div class="container-scroller">
-    
-      <!-- partial:partials/_navbar.html -->
+
+
       <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-          <a class="navbar-brand brand-logo me-5" href="{{ route('index') }}"><img src="{{ asset('customer/assets/images/logo.png') }}" class="me-2" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="{{ route('index') }}"><img src="{{ asset('customer/assets/images/logo.png') }}" alt="logo" /></a>
+          <a class="navbar-brand brand-logo me-5" href="index.html"><img src="assets/images/logo.svg" class="me-2" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -93,98 +93,25 @@
               </div>
             </li>
             <li class="nav-item nav-profile dropdown">
-             
               <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                <img src="{{ asset('customer/assets/images/faces/face28.jpg') }}" alt="profile" />
+                <img src="assets/images/faces/face28.jpg" alt="profile" />
               </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-                <a class="dropdown-item" href="{{ route('customer.profile.edit') }}">
-                  <i class="ti-settings text-primary"></i>Profile Settings </a>
                 <a class="dropdown-item">
-                  
-                  <form method="POST" action="{{ route('logout') }}" style="display: inline;">
-                    @csrf
-           
-                    <button type="submit" class="btn p-0"><i class="ti-power-off text-primary"></i>Logout</button>
-                </form>
+                  <i class="ti-settings text-primary"></i> Settings </a>
+                <a class="dropdown-item">
+                  <i class="ti-power-off text-primary"></i> Logout </a>
               </div>
             </li>
-          
+            <li class="nav-item nav-settings d-none d-lg-flex">
+              <a class="nav-link" href="#">
+                <i class="icon-ellipsis"></i>
+              </a>
+            </li>
           </ul>
           <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
             <span class="icon-menu"></span>
           </button>
         </div>
       </nav>
-      <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <!-- partial:partials/_sidebar.html -->
-        <nav class="sidebar sidebar-offcanvas" id="sidebar">
-  <ul class="nav">
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('customer.dashboard') }}">
-        <i class="icon-grid menu-icon"></i>
-        <span class="menu-title">Dashboard</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-        <i class="icon-layout menu-icon"></i>
-        <span class="menu-title">Profile</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="ui-basic">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="{{ route('customer.profile.edit') }}">Edit Profile</a></li>
-        </ul>
-      </div>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link"  href="{{ route('customer.pets.index') }}">
-          <i class="icon-paper menu-icon"></i>
-          <span class="menu-title">Pet Details</span>
-        </a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-        <i class="icon-columns menu-icon"></i>
-        <span class="menu-title">Bookings</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="form-elements">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"><a class="nav-link" href="#">Upcoming Bookings</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Past Bookings</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-        <i class="icon-bar-graph menu-icon"></i>
-        <span class="menu-title">Orders</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="charts">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="#">My Orders</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Order Details</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="icon-paper menu-icon"></i>
-        <span class="menu-title">Feedback & Reviews</span>
-      </a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" href="#">
-        <i class="icon-paper menu-icon"></i>
-        <span class="menu-title">Support</span>
-      </a>
-    </li>
-  </ul>
-</nav>
+    
