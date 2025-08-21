048acec9047b615c50f139990f87ddf3c7b5e9aa
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 16:14:04 +0530

    customer dashboard

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -132,8 +132,8 @@
       </a>
       <div class="collapse" id="ui-basic">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="{{ route('profile.edit') }}">Edit Profile</a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{ route('pets.index') }}">Pet Details</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{ route('customer.profile.edit') }}">Edit Profile</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{ route('customer.pets.index') }}">Pet Details</a></li>
         </ul>
       </div>
     </li>
