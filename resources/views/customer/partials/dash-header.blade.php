4cb98efab89c5d1f8599fe772a1249b80611069a
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 17:18:48 +0530

    customer dashboard

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -139,10 +139,12 @@
       <div class="collapse" id="ui-basic">
         <ul class="nav flex-column sub-menu">
           <li class="nav-item"> <a class="nav-link" href="{{ route('customer.profile.edit') }}">Edit Profile</a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{ route('customer.pets.index') }}">Pet Details</a></li>
         </ul>
       </div>
     </li>
+    
+   <li class="nav-item"> <a class="nav-link" href="{{ route('customer.pets.index') }}">Pet Details</a></li>
+
     <li class="nav-item">
       <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
         <i class="icon-columns menu-icon"></i>
