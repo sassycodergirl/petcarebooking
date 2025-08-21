9c6ae0477d784074656a00ca5a9a9b5a554309eb
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 20:23:36 +0530

    customer dashboard

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -30,8 +30,8 @@
 
       <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-          <a class="navbar-brand brand-logo me-5" href="index.html"><img src="assets/images/logo.svg" class="me-2" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+          <a class="navbar-brand brand-logo me-5" href="{{ route('index') }}"><img src="{{ asset('customer/assets/images/logo.png') }}" class="me-2" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="{{ route('index') }}"><img src="{{ asset('customer/assets/images/logo.png') }}" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -93,28 +93,30 @@
               </div>
             </li>
             <li class="nav-item nav-profile dropdown">
+             
               <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                <img src="assets/images/faces/face28.jpg" alt="profile" />
+                <img src="{{ asset('customer/assets/images/faces/face28.jpg') }}" alt="profile" />
               </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                <a class="dropdown-item" href="{{ route('customer.profile.edit') }}">
+                  <i class="ti-settings text-primary"></i>Profile Settings </a>
                 <a class="dropdown-item">
-                  <i class="ti-settings text-primary"></i> Settings </a>
-                <a class="dropdown-item">
-                  <i class="ti-power-off text-primary"></i> Logout </a>
+                  
+                  <form method="POST" action="{{ route('logout') }}" style="display: inline;">
+                    @csrf
+           
+                    <button type="submit" class="btn p-0"><i class="ti-power-off text-primary"></i>Logout</button>
+                </form>
               </div>
             </li>
-            <li class="nav-item nav-settings d-none d-lg-flex">
-              <a class="nav-link" href="#">
-                <i class="icon-ellipsis"></i>
-              </a>
-            </li>
+          
           </ul>
           <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
             <span class="icon-menu"></span>
           </button>
         </div>
       </nav>
-
+      <!-- partial -->
        <div class="container-fluid page-body-wrapper">
         <!-- partial:partials/_sidebar.html -->
         <nav class="sidebar sidebar-offcanvas" id="sidebar">
