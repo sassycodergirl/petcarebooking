ead5c8d647440094dd2ee6af70b03f67b4c33795
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 16:28:49 +0530

    customer dashboard

--- resources/views/customer/partials/dash-footer.blade.php
+++ resources/views/customer/partials/dash-footer.blade.php
@@ -4,8 +4,8 @@
        <!-- partial:partials/_footer.html -->
 <footer class="footer">
   <div class="d-sm-flex justify-content-center justify-content-sm-between">
-    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ms-1"></i></span>
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2025.All rights reserved.</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ms-1"></i>by <a href="https://coderbeans.com/">CoderBeans</a></span>
   </div>
 </footer>
     <!-- container-scroller -->
