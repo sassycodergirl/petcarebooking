f48466d66d116e27378b4de949cadff26490d58e
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-21 19:59:30 +0530

    customer dashboard

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -95,7 +95,7 @@
             <li class="nav-item nav-profile dropdown">
               @php
                   $user = Auth::user();
-                  $profilePhoto = $user->profile_photo ? asset('public' . $user->profile_photo) : asset('customer/assets/images/faces/face28.jpg');
+                  $profilePhoto = $user->profile_photo ? asset('public/' . $user->profile_photo) : asset('customer/assets/images/faces/face28.jpg');
               @endphp
               <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
                 <img src="{{ $profilePhoto }}" alt="profile" />
