f1491975f397381317415dddc17000cb06137e44
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-28 04:46:30 +0530

    customer dashboard

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -411,8 +411,8 @@
                     <div class="text-center">
                              <a class="navbar-brand" href="/"><img src="{{asset('images/logo.png')}}" alt></a>
                      </div>
-                     <div class="text-center">
-                        <h3 class="text-white">Welcome to Furry & Friends </h3>
+                     <div class="text-center mt-4">
+                        <h3 class="text-white mb-1">Welcome to Furry & Friends </h3>
                         <h4 class="text-white">Register to avail the best deals!</h4>
                      </div>
                      <div class="row">
