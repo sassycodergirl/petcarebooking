52e0ed255edae40d69b8fd8bcb276595824de7c0
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-30 13:45:58 +0530

    otp

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -507,7 +507,7 @@
                     </div>
 
                     <div id="loginError" class="text-danger mt-3"></div>
-                     <div class="text-center">
+                     <div class="text-center terms">
                             <p>I accept that I have read & understood Furry & Friends <a href="#">Privacy Policy</a> and <a href="#">T&Cs.</a></p>
                             <a href="{{ route('login') }}">Other Ways to Login</a>
                      </div>
