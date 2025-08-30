3fdcd6697ae2525bb568c38c8d7bd0a8ea582501
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-30 12:20:20 +0530

    otp

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+     'twilio' => [
+        'sid'        => env('TWILIO_SID'),
+        'token'      => env('TWILIO_AUTH_TOKEN'),
+        'verify_sid' => env('TWILIO_VERIFY_SID'),
+    ],
 
 ];
