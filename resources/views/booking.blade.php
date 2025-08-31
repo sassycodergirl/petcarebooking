abb9a0350dcd4908c27f949f6cb97879836e6199
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-31 12:50:18 +0530

    booking step

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -229,36 +229,36 @@
                             <!-- Left column: User details -->
                             <div class="col-md-8" id="reviewLeft">
                                 <div class="review-section">
-                                    <div class="booking-details-div">
-                                        <h4 class="review-title"><span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 1024 1024"><path fill="currentColor" d="m960 95.888l-256.224.001V32.113c0-17.68-14.32-32-32-32s-32 14.32-32 32v63.76h-256v-63.76c0-17.68-14.32-32-32-32s-32 14.32-32 32v63.76H64c-35.344 0-64 28.656-64 64v800c0 35.343 28.656 64 64 64h896c35.344 0 64-28.657 64-64v-800c0-35.329-28.656-63.985-64-63.985m0 863.985H64v-800h255.776v32.24c0 17.679 14.32 32 32 32s32-14.321 32-32v-32.224h256v32.24c0 17.68 14.32 32 32 32s32-14.32 32-32v-32.24H960zM736 511.888h64c17.664 0 32-14.336 32-32v-64c0-17.664-14.336-32-32-32h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32m0 255.984h64c17.664 0 32-14.32 32-32v-64c0-17.664-14.336-32-32-32h-64c-17.664 0-32 14.336-32 32v64c0 17.696 14.336 32 32 32m-192-128h-64c-17.664 0-32 14.336-32 32v64c0 17.68 14.336 32 32 32h64c17.664 0 32-14.32 32-32v-64c0-17.648-14.336-32-32-32m0-255.984h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32h64c17.664 0 32-14.336 32-32v-64c0-17.68-14.336-32-32-32m-256 0h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32h64c17.664 0 32-14.336 32-32v-64c0-17.68-14.336-32-32-32m0 255.984h-64c-17.664 0-32 14.336-32 32v64c0 17.68 14.336 32 32 32h64c17.664 0 32-14.32 32-32v-64c0-17.648-14.336-32-32-32"/></svg></span>Booking Details</h4>
+                                    <div class="booking-details-div bg-white p-3 p-md-5 box-shadow">
+                                        <h4 class="review-title"><span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="m960 95.888l-256.224.001V32.113c0-17.68-14.32-32-32-32s-32 14.32-32 32v63.76h-256v-63.76c0-17.68-14.32-32-32-32s-32 14.32-32 32v63.76H64c-35.344 0-64 28.656-64 64v800c0 35.343 28.656 64 64 64h896c35.344 0 64-28.657 64-64v-800c0-35.329-28.656-63.985-64-63.985m0 863.985H64v-800h255.776v32.24c0 17.679 14.32 32 32 32s32-14.321 32-32v-32.224h256v32.24c0 17.68 14.32 32 32 32s32-14.32 32-32v-32.24H960zM736 511.888h64c17.664 0 32-14.336 32-32v-64c0-17.664-14.336-32-32-32h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32m0 255.984h64c17.664 0 32-14.32 32-32v-64c0-17.664-14.336-32-32-32h-64c-17.664 0-32 14.336-32 32v64c0 17.696 14.336 32 32 32m-192-128h-64c-17.664 0-32 14.336-32 32v64c0 17.68 14.336 32 32 32h64c17.664 0 32-14.32 32-32v-64c0-17.648-14.336-32-32-32m0-255.984h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32h64c17.664 0 32-14.336 32-32v-64c0-17.68-14.336-32-32-32m-256 0h-64c-17.664 0-32 14.336-32 32v64c0 17.664 14.336 32 32 32h64c17.664 0 32-14.336 32-32v-64c0-17.68-14.336-32-32-32m0 255.984h-64c-17.664 0-32 14.336-32 32v64c0 17.68 14.336 32 32 32h64c17.664 0 32-14.32 32-32v-64c0-17.648-14.336-32-32-32"/></svg></span>Booking Details</h4>
                                         <div class="row">
                                             <div class="col-12 col-md-6">
-                                                <p><strong>Location:</strong></p> <div id="reviewLocation"></div>
+                                                <h5><strong>Location:</strong></h5> <div id="reviewLocation"></div>
                                             </div>
                                             <div class="col-12 col-md-6">
-                                                 <p><strong>Booking Type:</strong> </p><div id="reviewBookingType"></div>
+                                                 <h5><strong>Booking Type:</strong> </h5><div id="reviewBookingType"></div>
                                             </div>
                                             <div class="col-12 col-md-6">
-                                                 <p><strong>Check In Date & Time:</strong> </p><div id="reviewCheckInDateTime"></div>
+                                                 <h5><strong>Check In Date & Time:</strong> </h5><div id="reviewCheckInDateTime"></div>
                                             </div>
                                             <div class="col-12 col-md-6">
-                                                 <p><strong>Checo Out Date & Time:</strong> </p><div id="reviewCheckOutDateTime"></div>
+                                                 <h5><strong>Checo Out Date & Time:</strong></h5><div id="reviewCheckOutDateTime"></div>
                                             </div>
                                             
                                         </div>
                                    
                                     </div>
 
-                                    <div class="pet-details-div">
+                                    <div class="pet-details-div bg-white p-3 p-md-5 box-shadow">
                                         <h4>Pet Details</h4>
                                         <div id="reviewPets"></div>
                                     </div>
 
-                                    <div class="owner-details-div">
+                                    <div class="owner-details-div bg-white p-3 p-md-5 box-shadow">
                                         <h4 class="review-title">Owner Details</h4>
-                                        <p><strong>Name:</strong> <span id="reviewOwnerName"></span></p>
-                                        <p><strong>Contact:</strong> <span id="reviewOwnerContact"></span></p>
-                                        <p><strong>Address:</strong> <span id="reviewOwnerAddress"></span></p>
+                                        <h5><strong>Name:</strong> <span id="reviewOwnerName"></span></h5>
+                                        <h5><strong>Contact:</strong> <span id="reviewOwnerContact"></span></h5>
+                                        <h5><strong>Address:</strong> <span id="reviewOwnerAddress"></span></h5>
                                     </div>
                                 </div>
                             </div>
