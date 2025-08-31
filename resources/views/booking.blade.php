0c2537a6111281fa6dc0d1870999a971b7c052a2
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-08-31 18:27:31 +0530

    booking step

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -239,7 +239,7 @@
                                 <!-- Left column: User details -->
                                 <div class="col-md-8" id="reviewLeft">
                                     <div class="review-section">
-                                        <div class="booking-details-div bg-white p-3 p-md-5 box-shadow border rounded mb-4">
+                                        <div class="booking-details-div bg-white p-3 p-md-4 box-shadow border rounded mb-4">
                                             <h4 class="review-title"><span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M2 19c0 1.7 1.3 3 3 3h14c1.7 0 3-1.3 3-3v-8H2zM19 4h-2V3c0-.6-.4-1-1-1s-1 .4-1 1v1H9V3c0-.6-.4-1-1-1s-1 .4-1 1v1H5C3.3 4 2 5.3 2 7v2h20V7c0-1.7-1.3-3-3-3"/></svg></span>Booking Details</h4>
                                             <div class="row">
                                                 <div class="col-12 col-md-6 mb-4">
@@ -259,12 +259,12 @@
                                     
                                         </div>
 
-                                        <div class="pet-details-div bg-white p-3 p-md-5 box-shadow border rounded mb-4">
+                                        <div class="pet-details-div bg-white p-3 p-md-4 box-shadow border rounded mb-4">
                                             <h4 class="review-title"><span class="me-2"> <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M11.9 8.4c1.3 0 2.1-1.9 2.1-3.1c0-1-.5-2.2-1.5-2.2c-1.3 0-2.1 1.9-2.1 3.1c0 1 .5 2.2 1.5 2.2m-3.8 0c1 0 1.5-1.2 1.5-2.2C9.6 4.9 8.8 3 7.5 3C6.5 3 6 4.2 6 5.2c-.1 1.3.7 3.2 2.1 3.2m7.4-1c-1.3 0-2.2 1.8-2.2 3.1c0 .9.4 1.8 1.3 1.8c1.3 0 2.2-1.8 2.2-3.1c0-.9-.5-1.8-1.3-1.8m-8.7 3.1c0-1.3-1-3.1-2.2-3.1c-.9 0-1.3.9-1.3 1.8c0 1.3 1 3.1 2.2 3.1c.9 0 1.3-.9 1.3-1.8m3.2-.2c-2 0-4.7 3.2-4.7 5.4c0 1 .7 1.3 1.5 1.3c1.2 0 2.1-.8 3.2-.8c1 0 1.9.8 3 .8c.8 0 1.7-.2 1.7-1.3c0-2.2-2.7-5.4-4.7-5.4"/></svg></span>Pet Details</h4>
                                             <div class="row" id="reviewPets"></div>
                                         </div>
 
-                                        <div class="owner-details-div bg-white p-3 p-md-5 box-shadow border rounded mb-4">
+                                        <div class="owner-details-div bg-white p-3 p-md-4 box-shadow border rounded mb-4">
                                             <h4 class="review-title"><span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 12.75c3.942 0 7.987 2.563 8.249 7.712a.75.75 0 0 1-.71.787c-2.08.106-11.713.171-15.077 0a.75.75 0 0 1-.711-.787C4.013 15.314 8.058 12.75 12 12.75m0-9a3.75 3.75 0 1 0 0 7.5a3.75 3.75 0 0 0 0-7.5"/></svg></span>Owner Details</h4>
                                             <div class="row">
                                                 <div class="col-12 col-md-6 mb-4">
