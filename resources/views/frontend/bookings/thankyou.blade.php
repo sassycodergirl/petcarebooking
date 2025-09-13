e62080f1d1f3b803370300dcb24b9f1fa756cd08
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-14 02:15:23 +0530

    thankyou page

--- resources/views/frontend/bookings/thankyou.blade.php
+++ resources/views/frontend/bookings/thankyou.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="inner-bn-all">
                
-                <h1>Thank You!!</h1>
+                <h1>Thank You!</h1>
             </div>
         </div>
         <img src="{{ asset('images/dog-banner-img.png') }}" class="dog-banner-img" alt="">
@@ -19,28 +19,134 @@
 <div class="thank-you-page text-center py-5">
 
   
-    <div>
-        <h3><span class="me-3"> <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 16 16"><path fill="#37a722" d="M11.4 6.85a.5.5 0 0 0-.707-.707l-3.65 3.65l-1.65-1.65a.5.5 0 0 0-.707.707l2 2a.5.5 0 0 0 .707 0l4-4z"/><path fill="#37a722" fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8s3.58 8 8 8s8-3.58 8-8s-3.58-8-8-8M1 8c0-3.87 3.13-7 7-7s7 3.13 7 7s-3.13 7-7 7s-7-3.13-7-7" clip-rule="evenodd"/></svg></span>Payment Done Sucessfully!</h3>
-    </div>
-    <div>
-        <p>Your booking Request sent successfully and is currently <strong>pending approval</strong>.</p>
-        <p>You will receive updates on <strong>WhatsApp</strong>.  
-        Once approved by admin, you will receive a confirmation message.</p>
-    </div>
-    
    
 
-    <div class="booking-info">
-        <h3>Booking Details:</h3>
-        <ul>
-            <li><strong>Booking ID:</strong> {{ $booking->id }}</li>
-            <li><strong>Check In:</strong> {{ $booking->check_in->format('d M Y, h:i A') }}</li>
-            <li><strong>Check Out:</strong> {{ $booking->check_out->format('d M Y, h:i A') }}</li>
-            <li><strong>Status:</strong> Pending</li>
-        </ul>
+    <!-- From Uiverse.io by akshat-patel28 --> 
+    <div class="success-card">
+    <svg class="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+        <path
+        d="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"
+        fill-opacity="1"
+        ></path>
+    </svg>
+
+    <div class="icon-container">
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        stroke-width="0"
+        fill="currentColor"
+        stroke="currentColor"
+        class="icon"
+        >
+        <path
+            d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"
+        ></path>
+        </svg>
+    </div>
+    <div class="message-text-container">
+        <p class="message-text">Payment Done Sucessfully!</p>
+        <p class="sub-text">Your booking Request sent successfully.</p>
+    </div>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 15 15"
+        stroke-width="0"
+        fill="none"
+        stroke="currentColor"
+        class="cross-icon"
+    >
+        <path
+        fill="currentColor"
+        d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        ></path>
+    </svg>
     </div>
 
-    <a href="{{ route('customer.dashboard') }}" class="btn btn-success">Go to Homepage</a>
+
+    <!-- From Uiverse.io by PriyanshuGupta28 --> 
+<div class="stepper-box">
+  <div class="stepper-step stepper-completed">
+    <div class="stepper-circle">
+      <svg
+        viewBox="0 0 16 16"
+        class="bi bi-check-lg"
+        fill="currentColor"
+        height="16"
+        width="16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"
+        ></path>
+      </svg>
+    </div>
+    <div class="stepper-line"></div>
+    <div class="stepper-content">
+      <div class="stepper-title">Booking Request Submitted</div>
+      <div class="stepper-status">Completed</div>
+      <div class="stepper-time">{{ $booking->created_at->format('M d, h:i A') }}</div>
+    </div>
+  </div>
+
+  <div class="stepper-step stepper-active">
+    <div class="stepper-circle">2</div>
+    <div class="stepper-line"></div>
+    <div class="stepper-content">
+      <div class="stepper-title">Booking Confirmation on <strong>WhatsApp</strong> with Pending Status</div>
+      <div class="stepper-status">Pending Approval</div>
+
+    </div>
+  </div>
+
+  <div class="stepper-step stepper-pending">
+    <div class="stepper-circle">3</div>
+    <div class="stepper-content">
+      <div class="stepper-title">Confirmation by Admin through WhatsApp</div>
+      <div class="stepper-status">Pending</div>
+      <div class="stepper-time">Estimated: 30 min</div>
+    </div>
+  </div>
+
+  <div class="stepper-controls">
+    <button class="stepper-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-arrow-left"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+        ></path>
+      </svg>
+      Previous
+    </button>
+    <button class="stepper-button stepper-button-primary">
+      Next
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-arrow-right"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+        ></path>
+      </svg>
+    </button>
+  </div>
+</div>
+
+    <a href="{{ route('customer.dashboard') }}" class="btn btn-success">Go to Dashboard</a>
 </div>
 
 </div>
