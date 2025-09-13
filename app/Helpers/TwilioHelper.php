a7b0269cd9e4cfcfb41ad21a6c605e4f86f81e5b
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-13 16:26:38 +0530

    new Twilio Helper

--- app/Helpers/TwilioHelper.php
+++ app/Helpers/TwilioHelper.php
@@ -6,7 +6,20 @@ use Twilio\Rest\Client;
 
 class TwilioHelper
 {
-    public static function sendWhatsApp($to, $message)
+      public static function sendWhatsApp($to, $message)
     {
+        $sid   = env('TWILIO_SID');        // Your Twilio Account SID
+        $token = env('TWILIO_AUTH_TOKEN'); // Your Twilio Auth Token
+        $from  = env('TWILIO_WHATSAPP_FROM'); // E.g. 'whatsapp:+14155238886'
+
+        $twilio = new Client($sid, $token);
+
+        return $twilio->messages->create(
+            "whatsapp:{$to}", // Recipient
+            [
+                "from" => $from,
+                "body" => $message,
+            ]
+        );
     }
 }
