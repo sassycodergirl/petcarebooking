f4c495d470ddd52ee90229edf416018eb5e0f072
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-13 17:47:12 +0530

    whatsapp notification added

--- routes/web.php
+++ routes/web.php
@@ -171,6 +171,7 @@ Route::get('/slots/availability', [BookingController::class, 'getAvailability'])
 
 // Protected routes for logged-in users
 Route::middleware(['auth'])->group(function () {
-    Route::get('/bookings/thank-you/{id}', [BookingController::class, 'thankYou'])->name('bookings.thankyou');
+    Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
     Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
+    Route::get('/bookings/thank-you/{id}', [BookingController::class, 'thankYou'])->name('bookings.thankyou');
 });
