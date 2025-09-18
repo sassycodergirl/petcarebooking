b865c6e7f75d47f647baabd08ec312e36f30917e
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-19 03:05:49 +0530

    add to cart fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         </div>
                     </div>
                     <div class="remove-icon ms-3">
-                        <button class="remove-item" data-id="${id}">Remove</button>
+                        <button class="remove-item" data-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#d82b26" d="m20.37 8.91l-1 1.73l-12.13-7l1-1.73l3.04 1.75l1.36-.37l4.33 2.5l.37 1.37zM6 19V7h5.07L18 11v8a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2"/></svg></button>
                     </div>
                 </div>
             </div>`;
