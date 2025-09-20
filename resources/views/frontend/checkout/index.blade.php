8c9e2928024f422efcf8fc089c45afb19405c292
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 02:11:42 +0530

    checkout setip

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -221,6 +221,10 @@
   background: #f5f5f5;
   border: 1px solid #000;
 }
+.secure-text{
+      letter-spacing: 3px;
+          font-size: 12px;
+}
         @media (min-width: 768px) {
           .bd-placeholder-img-lg {
             font-size: 3.5rem;
@@ -242,7 +246,7 @@
             <a class="navbar-brand" href="{{ route('index') }}"><img src="{{asset('images/logo.png')}}" alt></a>
           </div>
           <div class="col-6 col-md-6 text-end">
-            <span class="secure-badge"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 3.94L12 .44L2 3.94V12c0 4.127 2.534 7.012 4.896 8.803a19.8 19.8 0 0 0 4.65 2.595q.17.064.342.122l.112.04l.114-.04a14 14 0 0 0 .65-.244a19.7 19.7 0 0 0 4.34-2.473C19.467 19.012 22 16.127 22 12zM11.001 15.415L6.76 11.172l1.414-1.415l2.828 2.829l5.657-5.657l1.415 1.414z"/></svg></span>
+            <span class="secure-badge"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#26d155" d="M22 3.94L12 .44L2 3.94V12c0 4.127 2.534 7.012 4.896 8.803a19.8 19.8 0 0 0 4.65 2.595q.17.064.342.122l.112.04l.114-.04a14 14 0 0 0 .65-.244a19.7 19.7 0 0 0 4.34-2.473C19.467 19.012 22 16.127 22 12zM11.001 15.415L6.76 11.172l1.414-1.415l2.828 2.829l5.657-5.657l1.415 1.414z"/></svg></span>
             <span class="secure-text">100% SECURE</span>
           </div>
         </div>
