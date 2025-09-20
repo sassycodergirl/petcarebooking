9d5fe8b237befb7c08e63d91b88fe39018b2d148
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 02:59:53 +0530

    checkout setip

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -645,9 +645,13 @@
 
                   <div class="col-12">
                     <div class="input-group">
-                      <input type="tel" class="input form-control" id="phone" name="billing_phone" autocomplete="off" required pattern="[0-9]{10}">
-                      <label class="user-label" for="billing_phone">Phone</label>
+                      <input type="tel" class="input form-control" id="phone" name="billing_phone" autocomplete="off" placeholder=" " pattern="[0-9]{10}">
+                      <label class="user-label" for="billing_phone">Phone (optional)</label>
                       <div class="invalid-feedback">Please enter a valid Phone Number.</div>
+                          <!-- Tooltip icon -->
+                        <span class="tooltip-icon" data-bs-toggle="tooltip" title="In case we need to contact you about your order">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 12 12"><path fill="#7a7a7a" d="M5.82 3c-.328 0-.61.15-.74.508c-.094.26-.299.492-.575.492s-.5-.172-.5-.498c0-.825.946-1.5 1.82-1.5c1.19 0 2.04 1.12 1.76 2.26a1.83 1.83 0 0 1-.48.849l-.448.448a.5.5 0 0 0-.146.354v.586a.5.5 0 0 1-1 0v-.586c0-.398.158-.779.439-1.06l.448-.448a.82.82 0 0 0-.572-1.404zm.93 5.75a.75.75 0 1 1-1.5 0a.75.75 0 0 1 1.5 0"/><path fill="#7a7a7a" fill-rule="evenodd" d="M12 6c0 3.31-2.69 6-6 6S0 9.31 0 6s2.69-6 6-6s6 2.69 6 6m-1 0c0 2.76-2.24 5-5 5S1 8.76 1 6s2.24-5 5-5s5 2.24 5 5" clip-rule="evenodd"/></svg>
+                        </span>
                     </div>
                   </div>
                 </div>
