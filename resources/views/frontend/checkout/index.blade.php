9deb1f2fd1186d4bee27584f28d856a18f8a3bf0
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-20 19:24:53 +0530

    checkout setup

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -135,8 +135,41 @@
     
         <div class="col-md-6 col-lg-6 p-md-5 p-4 border-right">
           <div class="form-div">
-            <h4 class="mb-3">Shipping & Delivery</h4>
+            
             <form class="needs-validation" novalidate>
+
+           
+            <div class="row">
+              <div class="col-6 col-md-6">
+                 <h4 class="mb-3">Contact</h4>
+              </div>
+              <div class="col-6 col-md-6">
+                <a href="javascript:void(0)">Sign In</a>
+              </div>
+            </div>
+
+            
+                <div class="col-12">
+                    <div class="input-group">
+                      <input type="email" class="input form-control" id="email" name="email" autocomplete="off" placeholder="you@example.com">
+                      <label for="email" class="user-label">Email <span class="text-muted">(Optional)</span></label>
+                      <div class="invalid-feedback">
+                        Please enter a valid email address.
+                      </div>
+                    </div>
+
+                    <div class="form-check mt-2">
+                      <input type="checkbox" class="form-check-input" id="updates" name="updates">
+                      <label class="form-check-label" for="updates">
+                        Keep me updated with the order updates and offers via WhatsApp, SMS and Email
+                      </label>
+                    </div>
+                  </div>
+
+
+
+
+            <h4 class="mb-3">Shipping & Delivery</h4>
               <div class="row g-3">
                 <div class="col-sm-6">
                     <div class="input-group">
@@ -149,12 +182,6 @@
                 </div>
 
                 <div class="col-sm-6">
-                  <!-- <label for="lastName" class="form-label">Last name</label> -->
-                  <!-- <input type="text" class="form-control" id="lastName" placeholder="Last Name" value="" required>
-                  <div class="invalid-feedback">
-                    Valid last name is required.
-                  </div> -->
-
                     <div class="input-group">
                      <input type="text" class="input form-control" id="lastName" name="lasttName" autocomplete="off" value="" required>
                         <label class="user-label">Last Name</label>
@@ -164,22 +191,9 @@
                     </div>
                 </div>
 
-<!-- 
-                <div class="col-12">
-                  <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                  <input type="email" class="form-control" id="email" placeholder="you@example.com">
-                  <div class="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
-                  </div>
-                </div> -->
+
 
                 <div class="col-12">
-                  <!-- <label for="address" class="form-label">Address</label>
-                  <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
-                  <div class="invalid-feedback">
-                    Please enter your shipping address.
-                  </div> -->
-
                    <div class="input-group">
                      <input type="text" class="input form-control" id="address" name="address" autocomplete="off" value="" required>
                         <label class="user-label">Last Name</label>
@@ -192,10 +206,8 @@
                 </div>
 
                 <div class="col-12">
-                  <!-- <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label> -->
-                  <!-- <input type="text" class="form-control" id="address2" placeholder="Apartment, suite, etc. (optional)"> -->
                    <div class="input-group">
-                     <input type="text" class="input form-control" id="address2" name="address-extra" autocomplete="off" value="" required>
+                     <input type="text" class="input form-control" id="address2" name="address-extra" autocomplete="off" value="">
                         <label class="user-label">Apartment, suite, etc. (optional)</label>
                     </div>
                 </div>
@@ -210,16 +222,7 @@
                   </div>
                 </div>
 
-                <!-- <div class="col-md-4">
-                  <label for="state" class="form-label">State</label>
-                  <select class="form-select" id="state" required>
-                    <option value="">Choose...</option>
-                    <option>California</option>
-                  </select>
-                  <div class="invalid-feedback">
-                    Please provide a valid state.
-                  </div>
-                </div> -->
+       
 
                 <div class="col-md-4">
                     <div class="input-group">
