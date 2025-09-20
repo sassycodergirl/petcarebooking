39a8f7c94cabc5bb9d0d98da4bb7a6bc74379dde
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-20 06:54:38 +0530

    add to cart fix

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -1,7 +1,7 @@
 @include('partials.header')
 
 <section class="banner inner-banner">
-    <div class=" py-2 py-md-5 product-container">
+    <div class=" py-4 py-md-5 product-container">
         <div class="container-fluid px-md-5">
             <div class="row">
                 <div class="col-12 col-md-6">
