3dd5bcf6ab3546067c29586ccaa254083611df8e
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-22 03:05:27 +0530

    order placing setup

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -43,14 +43,14 @@
                 <div class="card tale-bg">
                   <div class="card-people mt-auto">
                     <img src="{{ asset('assets/images/dashboard/people.svg') }}" >
-                    <div class="weather-info">
+                    <!-- <div class="weather-info">
                       <div class="d-flex">
                         <div>
                           <h2 class="mb-0 font-weight-normal"><i class="icon-sun me-2"></i>31<sup>C</sup></h2>
                         </div>
                       
                       </div>
-                    </div>
+                    </div> -->
                   </div>
                 </div>
               </div>
@@ -58,7 +58,7 @@
                 <div class="row">
                   <div class="col-md-6 mb-4 stretch-card transparent">
                     <div class="card card-tale" >
-                      <div class="card-body" style="visiblity:hidden;">
+                      <div class="card-body" style="opacity: 0;">
                         <p class="mb-4">Today’s Bookings</p>
                         <p class="fs-30 mb-2">4006</p>
                         <p>10.00% (30 days)</p>
@@ -67,7 +67,7 @@
                   </div>
                   <div class="col-md-6 mb-4 stretch-card transparent">
                     <div class="card card-dark-blue">
-                      <div class="card-body" style="visiblity:hidden;">
+                      <div class="card-body" style="opacity: 0;">
                         <p class="mb-4">Total Bookings</p>
                         <p class="fs-30 mb-2">61344</p>
                         <p>22.00% (30 days)</p>
@@ -78,7 +78,7 @@
                 <div class="row">
                   <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
                     <div class="card card-light-blue">
-                      <div class="card-body" style="visiblity:hidden;">
+                      <div class="card-body" style="opacity: 0;">
                         <p class="mb-4">Number of Meetings</p>
                         <p class="fs-30 mb-2">34040</p>
                         <p>2.00% (30 days)</p>
@@ -87,7 +87,7 @@
                   </div>
                   <div class="col-md-6 stretch-card transparent">
                     <div class="card card-light-danger">
-                      <div class="card-body" style="visiblity:hidden;">
+                      <div class="card-body" style="opacity: 0;">
                         <p class="mb-4">Number of Clients</p>
                         <p class="fs-30 mb-2">47033</p>
                         <p>0.22% (30 days)</p>
