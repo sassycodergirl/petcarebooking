f752d2c325761ba87e1df0d13b0e91b450490e20
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-22 02:31:57 +0530

    order placing setup

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
 
-class AdminOrderController extends Controller
+class OrderController extends Controller
 {
     /**
      * List all orders
