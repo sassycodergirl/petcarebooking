055d35d19de8b6814ae8042644ec2fc0d7f67fac
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 20:26:52 +0530

    checkout order placement

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -180,7 +180,7 @@ window.addEventListener("load", function () {
                                   <span class="me-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 7.5a3 3 0 1 1-6 0a3 3 0 0 1 6 0m4.5 13c-.475-9.333-14.525-9.333-15 0"/></svg></span>  Account
                                 </a>
                             @else
-                                <button class="btn btn-primary" role="button"
+                                <button class="btn login-head-btn" role="button"
                                         data-bs-toggle="modal"
                                         data-bs-target="#loginModal"
                                         style="cursor: pointer;">
