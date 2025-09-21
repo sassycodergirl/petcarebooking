2723efc34ad9eee8520133bb053a1743c3cf44b5
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 20:22:16 +0530

    checkout order placement

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -161,16 +161,7 @@ window.addEventListener("load", function () {
                                 <span class="btn-closes"><i class="fa-solid fa-xmark"></i></span></li>
                           <li>
                               <a href="#" class="cd-button cart-btn">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 512 512">
-                                      <circle cx="176" cy="416" r="16" fill="none" stroke="currentColor" stroke-linecap="round"
-                                          stroke-linejoin="round" stroke-width="32" />
-                                      <circle cx="400" cy="416" r="16" fill="none" stroke="currentColor" stroke-linecap="round"
-                                          stroke-linejoin="round" stroke-width="32" />
-                                      <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="32" d="M48 80h64l48 272h256" />
-                                      <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="32" d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128" />
-                                  </svg>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5"><path d="M20.232 10.526c-.585-3.121-.878-4.682-1.989-5.604C17.133 4 15.545 4 12.37 4h-.721c-3.176 0-4.763 0-5.874.922s-1.404 2.483-1.99 5.604c-.822 4.389-1.234 6.583-.034 8.029S7.182 20 11.648 20h.721c4.465 0 6.698 0 7.898-1.445c.696-.84.85-1.93.695-3.555"/><path d="M9.17 8a3.001 3.001 0 0 0 5.66 0"/></g></svg>
 
                                   @php
                                       $cart = session('cart', []);
