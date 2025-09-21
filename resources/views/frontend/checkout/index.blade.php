022b0c3c435772c2e34e52e3096f1c57a156e92c
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 22:32:43 +0530

    order placing setup

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -866,7 +866,7 @@ select.input:valid ~ .user-label { /* <-- Changed :not([value=""]) to :valid */
 
                   // Generate hidden inputs for form submission
                   hiddenInputsContainer.insertAdjacentHTML('beforeend', `
-                  <input type="hidden" name="cart[${index}][id]" value="${item.id}" data-key="${item.key}">
+                  <input type="hidden" name="cart[${index}][id]" value="${item.product_id}" data-key="${item.key}">
                   <input type="hidden" name="cart[${index}][qty]" value="${item.qty}" data-key="${item.key}">
                   <input type="hidden" name="cart[${index}][size]" value="${item.size ?? ''}" data-key="${item.key}">
                   <input type="hidden" name="cart[${index}][color]" value="${item.color_name ?? ''}" data-key="${item.key}">
