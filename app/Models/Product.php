02fa831362473b1a4e70d283c0ac04996fd79473
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-21 22:10:26 +0530

    order placing setup

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -41,4 +41,9 @@ class Product extends Model
     {
         return $this->variants()->exists();
     }
+
+        public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
