6f6732be528217e9c2f178dd2444f1b827336646
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-22 03:06:27 +0530

    order placing setup

--- resources/views/customer/partials/dash-header.blade.php
+++ resources/views/customer/partials/dash-header.blade.php
@@ -56,16 +56,16 @@
               </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
                 <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
-                <a class="dropdown-item preview-item">
+                <!-- <a class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
                     <div class="preview-icon bg-success">
                       <i class="ti-info-alt mx-0"></i>
                     </div>
                   </div>
-                  <!-- <div class="preview-item-content">
+                  <div class="preview-item-content">
                     <h6 class="preview-subject font-weight-normal">Application Error</h6>
                     <p class="font-weight-light small-text mb-0 text-muted"> Just now </p>
-                  </div> -->
+                  </div>
                 </a>
                 <a class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
@@ -88,7 +88,7 @@
                     <h6 class="preview-subject font-weight-normal">New user registration</h6>
                     <p class="font-weight-light small-text mb-0 text-muted"> 2 days ago </p>
                   </div>
-                </a>
+                </a> -->
               </div>
             </li>
             <li class="nav-item nav-profile dropdown">
