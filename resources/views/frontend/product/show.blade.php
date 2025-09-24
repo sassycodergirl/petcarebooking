8dab1d2ed321023a4fa0b6841c5bec4a26b92bca
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-25 01:37:59 +0530

    product managemnet

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -70,22 +70,29 @@
                                     </div>
                                 @endif
 
-                                @if($product->variants->pluck('color')->filter()->count())
-                                @php
-                                    $hasStock = $product->variants->where('color_id', $color->id)->sum('stock_quantity') > 0;
-                                @endphp
-                                    <div class="mb-2">
-                                        <label class="form-label">Colors:</label>
-                                        <div class="selectgroup selectgroup-pills color-pills {{ !$hasStock ? 'disabled' : '' }}">
-                                            @foreach($product->variants->pluck('color')->filter()->unique('id') as $color)
-                                                <label class="selectgroup-item">
-                                                    <input type="radio" name="variant_color" value="{{ $color->id }}" class="selectgroup-input variant-select" {{ $loop->first ? 'checked' : '' }} {{ !$hasStock ? 'disabled' : '' }}>
-                                                    <span class="selectgroup-button" style="background-color: {{ $color->hex_code ?? '#ccc' }}; color: #fff;"></span>
-                                                </label>
-                                            @endforeach
-                                        </div>
+                              @if($product->variants->pluck('color')->filter()->count())
+                                <div class="mb-2">
+                                    <label class="form-label">Colors:</label>
+                                    <div class="selectgroup selectgroup-pills color-pills">
+                                        @foreach($product->variants->pluck('color')->filter()->unique('id') as $color)
+                                            @php
+                                                $hasStock = $product->variants->where('color_id', $color->id)->sum('stock_quantity') > 0;
+                                            @endphp
+                                            <label class="selectgroup-item {{ !$hasStock ? 'disabled' : '' }}">
+                                                <input type="radio"
+                                                    name="variant_color"
+                                                    value="{{ $color->id }}"
+                                                    class="selectgroup-input variant-select"
+                                                    {{ $loop->first ? 'checked' : '' }}
+                                                    {{ !$hasStock ? 'disabled' : '' }}>
+                                                <span class="selectgroup-button"
+                                                    style="background-color: {{ $color->hex_code ?? '#ccc' }}; color: #fff;"></span>
+                                            </label>
+                                        @endforeach
                                     </div>
-                                @endif
+                                </div>
+                            @endif
+
                             </div>
                         @endif
 
