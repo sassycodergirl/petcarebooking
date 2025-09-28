d05256776b752e2f361bfa4d54fa3a1f0430e547
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-28 16:29:51 +0530

    product managemnet

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -1,18 +1,32 @@
 @include('partials.header')
 
-<section class="banner inner-banner container-fluid px-4 px-md-5">
-    <div class="js-product-banner">
-        <div class="product-banner-col">
-            <div class="product-banner-image">
-                <img src="{{ asset('images/product-banner.jpg') }}" alt="">
-            </div>
-        </div>
-        <div class="product-banner-col">
-            <div class="product-banner-image">
-                <img src="{{ asset('images/product-banner.jpg') }}" alt="">
-            </div>
-        </div>
-    </div>
+
+
+@php
+    $breadcrumbs = [];
+    $current = $category;
+    while($current) {
+        $breadcrumbs[] = $current;
+        $current = $current->parent;
+    }
+    $breadcrumbs = array_reverse($breadcrumbs);
+@endphp
+<section class="banner inner-banner container-fluid px-md-5">
+      <nav aria-label="breadcrumb" class="mb-4">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('shop.index') }}">Home</a></li>
+            @foreach($breadcrumbs as $crumb)
+                @if($loop->last)
+                    <li class="breadcrumb-item active" aria-current="page">{{ $crumb->name }}</li>
+                @else
+                    <li class="breadcrumb-item">
+                        <a href="{{ route('shop.category', $crumb->slug) }}">{{ $crumb->name }}</a>
+                    </li>
+                @endif
+            @endforeach
+        </ol>
+    </nav>
+   
 </section>
 
 <section class="product-section">
