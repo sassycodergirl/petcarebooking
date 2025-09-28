670dbe8c8bbfeb42a5e0655d29bf34e9580a3118
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-28 19:14:33 +0530

    product managemnet

--- resources/views/frontend/shop/category.blade.php
+++ resources/views/frontend/shop/category.blade.php
@@ -73,7 +73,7 @@
             <ul class="list-group prdct-list">
                 {{-- Always show parent category as first item --}}
                 @if($category->parent)
-                    <li class=" {{ request()->url() == route('shop.category', $category->parent->slug) ? 'active' : '' }}">
+                    <li class="cat-pill cat-pill-top {{ request()->url() == route('shop.category', $category->parent->slug) ? 'active' : '' }}">
                         <a href="{{ route('shop.category', $category->parent->slug) }}" class="{{ request()->url() == route('shop.category', $category->parent->slug) ? 'text-white' : '' }}">
                             All {{ $category->parent->name }}
                         </a>
@@ -83,7 +83,7 @@
                 @if($category->parent)
                     {{-- If this is a child category, show all siblings sorted by name --}}
                     @foreach($category->parent->children->sortBy('name') as $sibling)
-                        <li class=" {{ $sibling->id === $category->id ? 'active' : '' }}">
+                        <li class="cat-pill text-black {{ $sibling->id === $category->id ? 'active' : '' }}">
                             <a href="{{ route('shop.category', $sibling->slug) }}" class="{{ $sibling->id === $category->id ? 'text-white' : '' }}">
                                 {{ $sibling->name }}
                             </a>
@@ -91,13 +91,13 @@
                     @endforeach
                 @elseif($subcategories->count())
                     {{-- If this is a parent category, show "All" + subcategories sorted by name --}}
-                    <li class=" {{ request()->url() == route('shop.category', $category->slug) ? 'active' : '' }}">
+                    <li class="cat-pill text-black {{ request()->url() == route('shop.category', $category->slug) ? 'active' : '' }}">
                         <a href="{{ route('shop.category', $category->slug) }}" class="{{ request()->url() == route('shop.category', $category->slug) ? 'text-white' : '' }}">
                             All {{ $category->name }}
                         </a>
                     </li>
                     @foreach($subcategories->sortBy('name') as $subcategory)
-                        <li class="{{ request()->url() == route('shop.category', $subcategory->slug) ? 'active' : '' }}">
+                        <li class="cat-pill text-black {{ request()->url() == route('shop.category', $subcategory->slug) ? 'active' : '' }}">
                             <a href="{{ route('shop.category', $subcategory->slug) }}" class="{{ request()->url() == route('shop.category', $subcategory->slug) ? 'text-white' : '' }}">
                                 {{ $subcategory->name }}
                             </a>
