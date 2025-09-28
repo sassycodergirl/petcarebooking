cf932601efeec2bd5505df108fc82cd3c7bbb336
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-28 16:31:28 +0530

    product managemnet

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -2,28 +2,11 @@
 
 
 
-@php
-    $breadcrumbs = [];
-    $current = $category;
-    while($current) {
-        $breadcrumbs[] = $current;
-        $current = $current->parent;
-    }
-    $breadcrumbs = array_reverse($breadcrumbs);
-@endphp
 <section class="banner inner-banner container-fluid px-md-5">
       <nav aria-label="breadcrumb" class="mb-4">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('shop.index') }}">Home</a></li>
-            @foreach($breadcrumbs as $crumb)
-                @if($loop->last)
-                    <li class="breadcrumb-item active" aria-current="page">{{ $crumb->name }}</li>
-                @else
-                    <li class="breadcrumb-item">
-                        <a href="{{ route('shop.category', $crumb->slug) }}">{{ $crumb->name }}</a>
-                    </li>
-                @endif
-            @endforeach
+            <li class="breadcrumb-item active" aria-current="page">collections</li>
         </ol>
     </nav>
    
