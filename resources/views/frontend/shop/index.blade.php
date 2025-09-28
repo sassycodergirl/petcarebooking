ab838f4a29e4c5f11497b0d47c9439fa08ee22ff
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-28 18:40:31 +0530

    product managemnet

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -13,7 +13,7 @@
     <div class="js-product-banner">
         <div class="product-banner-col">
             <div class="product-banner-image">
-                <img src="{{ asset('images/shop.webp') }}" alt="">
+                <img src="{{ asset('images/shop-b.webp') }}" alt="">
             </div>
         </div>
        
