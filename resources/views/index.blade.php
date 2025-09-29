061f9b86a2e881a3994d8b2bee410a6a90913f6c
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-29 22:59:16 +0530

    product managemnet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,7 +50,8 @@
 
                     <div class="banner-img">
                         <div class="banner-main">
-                            <img src="{{ asset('images/fashion-mob.webp') }}" class="third-banner" alt="">
+                            <img src="{{ asset('images/fashion.webp') }}" class="d-none d-md-block d-lg-block" alt="">
+                            <img src="{{ asset('images/fashion-mob.webp') }}" class="d-block d-md-none d-lg-none" alt="">
                         </div>
                        
                     </div>
