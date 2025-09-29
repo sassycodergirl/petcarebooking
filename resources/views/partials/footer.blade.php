7b97feaf9c578d25dcb412d64e2976129255240f
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-09-29 18:53:59 +0530

    product managemnet

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -74,7 +74,9 @@
         <img src="{{asset('images/footer-lft.png')}}" class="footer-lft" alt="">
         <img src="{{asset('images/footer-rgt.png')}}" class="footer-rgt" alt="">
     </footer>
-    <button id="backToTopBtn" class="scroll-btn"><img src="{{asset('images/back-to-top.svg')}}" alt=""></button>
+    <button id="backToTopBtn" class="scroll-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="2.5em" height="2.5em" viewBox="0 0 1024 1024"><path fill="#fff" d="M572.235 205.282v600.365a30.118 30.118 0 1 1-60.235 0V205.282L292.382 438.633a28.913 28.913 0 0 1-42.646 0a33.43 33.43 0 0 1 0-45.236l271.058-288.045a28.913 28.913 0 0 1 42.647 0L834.5 393.397a33.43 33.43 0 0 1 0 45.176a28.913 28.913 0 0 1-42.647 0l-219.618-233.23z"/></svg>
+    </button>
 
 
 
