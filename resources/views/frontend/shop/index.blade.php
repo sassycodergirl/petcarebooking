374fdb67d3575f85e5eb2461c1e4dfbb88235457
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-10-01 20:34:37 +0530

    product managemnet

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -91,33 +91,7 @@
                         </div>
                     </div>
 
-                    <!-- {{-- INDIVIDUAL PARENT CATEGORY TABS --}}
-                    @foreach($categories as $parent)
-                        <div class="tab-pane fade" id="cat-{{ $parent->id }}" role="tabpanel">
-                            <div class="row">
-                                @forelse($parent->children as $subcategory)
-                                    <div class="col-6 col-md-4 col-lg-4 col-sm-6">
-                                        <div class="product-card-col">
-                                            <a href="{{ route('shop.category', $subcategory->slug) }}" class="product-card-img">
-                                                @if($subcategory->image)
-                                                    <img src="{{ asset('public/' . $subcategory->image) }}" alt="{{ $subcategory->name }}">
-                                                @else
-                                                    <img src="{{ asset('images/default-category.png') }}" alt="{{ $subcategory->name }}">
-                                                @endif
-                                            </a>
-                                            <h3>
-                                                <a href="{{ route('shop.category', $subcategory->slug) }}">
-                                                    {{ $subcategory->name }}
-                                                </a>
-                                            </h3>
-                                        </div>
-                                    </div>
-                                @empty
-                                    <p>No subcategories under {{ $parent->name }}.</p>
-                                @endforelse
-                            </div>
-                        </div>
-                    @endforeach -->
+                    
 
                     {{-- INDIVIDUAL PARENT CATEGORY TABS --}}
                     @foreach($categories as $parent)
