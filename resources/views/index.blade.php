cd6862d1223e1df09c435617cc5c8cc9fcadc109
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-10-02 22:30:31 +0530

    product managemnet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -183,35 +183,35 @@
         </div>
     </section>
 
-    <section class="top-categories">
+    <section class="top-categories pb-5">
         <div class="container">
             <div class="product-hd">
                 <h2>Top Categories</h2>
                 <p>Everything your pet needs, in a few clicks!</p>
             </div>
             <div class="row">
-                <div class="col-6 col-md-4 mt-2 mt-md-4 px-2">
+                <div class="col-6 col-md-4 mt-2 mt-md-4 px-1 px-md-2">
                     <div>
                         <a>
                             <img class="img-fluid" src="{{ asset('images/traditional-cats.webp') }}" alt="">
                         </a>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 mt-2 mt-md-4 px-2">
+                <div class="col-6 col-md-4 mt-2 mt-md-4 px-1 px-md-2">
                     <div>
                         <a>
                             <img class="img-fluid" src="{{ asset('images/treats-cat.webp') }}" alt="">
                         </a>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 mt-2 mt-md-4 px-2">
+                <div class="col-6 col-md-4 mt-2 mt-md-4 px-1 px-md-2">
                     <div>
                         <a>
                             <img class="img-fluid" src="{{ asset('images/bows-cat.webp') }}" alt="">
                         </a>
                     </div>
                 </div>
-                <div class="col-6 col-md-6 mt-2 mt-md-4 px-2">
+                <div class="col-6 col-md-6 mt-2 mt-md-4 px-1 px-md-2">
                     <div>
                         <a>
                             <img class="img-fluid d-none d-md-block d-lg-block" src="{{ asset('images/bandana-cat.webp') }}" alt="">
@@ -220,7 +220,7 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 mt-2 mt-md-4 px-2">
+                <div class="col-12 col-md-6 mt-2 mt-md-4 px-1 px-md-2">
                     <div>
                         <a>
                             <img class="img-fluid" src="{{ asset('images/access-cat.webp') }}" alt="">
