fe9ec2afc78d2aa31e561355bd4eae355d14c2f7
Sudipa Naiya <sudipanaiya95@gmail.com>
2025-10-03 02:27:44 +0530

    product managemnet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -589,7 +589,7 @@
     </section>
 
 
-    <section class="dog-treats py-4">
+    <section class="dog-treats py-5">
         <div class="container">
             <div class="row">
                 <div class="col-md-5">
